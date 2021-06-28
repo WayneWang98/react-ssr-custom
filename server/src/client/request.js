4c609d22e52d4dc9f0016d0381c461ea55b61d8c
waynewang98 <1070546617@qq.com>
2021-06-28 22:23:13 +0800

    对java端校验使用的secret进行统一管理

--- server/src/client/request.js
+++ server/src/client/request.js
@@ -1,7 +1,11 @@
 import axios from 'axios'
+import config from '../config'
 
 const instance = axios.create({
-  baseURL: '/'
+  baseURL: '/',
+  params: {
+    secret: config.secret // 公用secret提取
+  }
 })
 
 export default instance
